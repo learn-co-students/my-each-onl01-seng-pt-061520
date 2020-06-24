ec3f8c242d7db59036b4a7fe4c1390fdedabeb01
“Megan <meganschertz@gmail.com>
2020-06-24 18:44:38 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+ i = 0
+ counter = 0
+ while i < array.length do 
+  yield(array[i])
+  i = i + 1
+ array.collect
+ counter += 1
+end
+return array
+
 end
